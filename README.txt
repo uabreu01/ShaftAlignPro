5cf4b42d4a0bdc4f8ca53a9bbe194f039b4c6760
uabreu01 <94654696+uabreu01@users.noreply.github.com>
2026-09-23 18:25:44 -0400

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,5 @@
+ShaftAlign Pro ABS - Android WebView Project
+Version 1.0
+
+La aplicacion carga localmente app/src/main/assets/index.html y funciona sin depender de un sitio web.
+Proyecto preparado para compilar como APK Android.
---
+++ app/src/main/java/com/abs/shaftalignpro/MainActivity.java
@@ -0,0 +1,38 @@
+package com.abs.shaftalignpro;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setBuiltInZoomControls(false);
+        s.setDisplayZoomControls(false);
+        s.setLoadWithOverviewMode(true);
+        s.setUseWideViewPort(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) webView.goBack();
+        else super.onBackPressed();
+    }
+}
